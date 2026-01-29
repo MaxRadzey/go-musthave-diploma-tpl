55c730656d86d049a528b4359ac9f42f4ef27957
Максим Радзей <max.radzey@yandex.ru>
2026-01-29 23:28:58 +0500

    iter1: добавлен конфиг

---
+++ internal/config/config.go
@@ -0,0 +1,41 @@
+package config
+
+import (
+	"flag"
+	"os"
+)
+
+type Config struct {
+	RunAddress           string
+	DatabaseDSN          string
+	AccrualSystemAddress string
+	LogLevel         string
+}
+
+func New() *Config {
+	return &Config{
+		RunAddress:       "localhost:8080",
+		LogLevel:         "info",
+		DatabaseDSN:      "postgres://shortener:shortener@localhost:5432/shortener",
+	}
+}
+
+func ParseEnv(config *Config) {
+	if RunAddress := os.Getenv("SERVER_ADDRESS"); RunAddress != "" {
+		config.RunAddress = RunAddress
+	}
+	if LogLevel := os.Getenv("LOG_LEVEL"); LogLevel != "" {
+		config.LogLevel = LogLevel
+	}
+	if DatabaseDSN := os.Getenv("DATABASE_DSN"); DatabaseDSN != "" {
+		config.DatabaseDSN = DatabaseDSN
+	}
+}
+
+func ParseFlags(config *Config) {
+	flag.StringVar(&config.RunAddress, "a", config.RunAddress, "address and port to run server")
+	flag.StringVar(&config.LogLevel, "l", config.LogLevel, "log level")
+	flag.StringVar(&config.DatabaseDSN, "d", config.DatabaseDSN, "database connection string")
+
+	flag.Parse()
+}
